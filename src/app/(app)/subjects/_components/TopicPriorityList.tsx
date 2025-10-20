180b403f8c08071d10dbe4555068606272b370cb
Anthony Pagano <z5688022@ad.unsw.edu.au>
2025-10-21 04:19:12 +1100

    fixes

--- src/app/(app)/subjects/_components/TopicPriorityList.tsx
+++ src/app/(app)/subjects/_components/TopicPriorityList.tsx
@@ -1,5 +1,5 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Progress } from "@/components/ui/progress";
+import { Card, CardContent, CardHeader, CardTitle } from "~/app/components/ui/card";
+import { Progress } from "~/app/components/ui/progress";
 
 type Topic = {
   id: string;

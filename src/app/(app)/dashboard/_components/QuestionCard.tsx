71c00891e291704dc44fd8981fa82495c14529d5
Suman-0077 <sumanthapachhetri06@gmail.com>
2025-10-21 02:51:16 +1100

    Adding SUbject components

--- src/app/(app)/dashboard/_components/QuestionCard.tsx
+++ src/app/(app)/dashboard/_components/QuestionCard.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 
-import { Card, CardContent, CardHeader, CardTitle } from "../ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "../../../components/ui/card";
 import { cn } from "~/lib/utils";
 
 const stubQuestion = {
